1ccb743421c4bae16f3861e1ab63aa67982f37a5
terranivium <wesleyscottuk@gmail.com>
2020-01-21 12:55:42 +0000

    Week2Lab complete

--- Week2Lab/Week2Test.java
+++ Week2Lab/Week2Test.java
@@ -1,14 +1,35 @@
 public class Week2Test{
 	public static void main(String[] args){
-		Date danielaDOB = new Date(25, 04, 1997);
-		Date wesleyDOB = new Date(25, 02, 1991);
+		Date danielaDOB = new Date(25, 4, 1997);
+		Date wesleyDOB = new Date(25, 2, 1991);
+		Date jodiDOB = new Date(04, 9, 1996);
+		Date paulineDOB = new Date(12, 9, 1964);
 
 		Person daniela = new Person("Daniela", danielaDOB);
 		Person wesley = new Person("Wesley", wesleyDOB);
+		Person jodi = new Person("Jodi", jodiDOB);
+		Person pauline = new Person("Pauline", paulineDOB);
 
 		Lop listOfPeople = new Lop();
 		listOfPeople.add(daniela);
 		listOfPeople.add(wesley);
+		listOfPeople.add(jodi);
+		listOfPeople.add(pauline);
+
+		listOfPeople.remove(jodi);
+		listOfPeople.remove(pauline);
+
+		System.out.println("-----------------");
+		System.out.println("Test .find method");
+		System.out.println("-----------------");
 		System.out.println(listOfPeople.find("Daniela"));
+		System.out.println(listOfPeople.find("Wesley"));
+		System.out.println(listOfPeople.find("Jodi"));
+		System.out.println(listOfPeople.find("Pauline"));
+
+		System.out.println("-----------------");
+		System.out.println("Test .print method");
+		System.out.println("-----------------");
+		listOfPeople.print();
 	}
 }
